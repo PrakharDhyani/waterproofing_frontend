7d71ae9a6f2ca7a7c97f1b59a015098ecaf4226d
AdityaCoder-N <91337266+AdityaCoder-N@users.noreply.github.com>
2022-12-29 13:02:43 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-        <h1>App</h1>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
