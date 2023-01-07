50984343e4d609bebf6421d3fdf418770e876bed
Prakhar Dhyani <prakhardhyani1000@gmail.com>
2023-01-07 19:23:21 +0530

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-xl bg-body-tertiary">
                 <div className="container-fluid">
-                    
                     <a className="navbar-brand ms-5" href="#">
                         < img src="./images/logo.png" alt="IMG" id='logo'/>
                     </a>
@@ -15,9 +14,7 @@ const Navbar = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
@@ -27,19 +24,16 @@ const Navbar = () => {
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Concepts</a>
                             </li>
-                            
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Services
                                 </a>
                                 <ul className="dropdown-menu animate slideIn">
-                                    
                                     <li><a className="dropdown-item" href="#">Water Proofing</a></li>
                                     <li><a className="dropdown-item" href="#">Epoxy Flooring / Protective Paints</a></li>
                                     <li><a className="dropdown-item" href="#">Repair and Rehabilation</a></li>
                                     <li><a className="dropdown-item" href="#">Insulation</a></li>
                                     <li><a className="dropdown-item" href="#">Project Consultancy</a></li>
-
                                 </ul>
                             </li>
                             
