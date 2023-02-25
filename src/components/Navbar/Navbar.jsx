7da33f25c21a2845770b07e5629ec4e422435d94
Prakhar Dhyani <prakhardhyani1000@gmail.com>
2023-02-26 00:38:32 +0530

    Navbar Edit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ navColor,bgColor,page }) => {
                     <a className="navbar-brand ms-5" href="/">
                         < img src="./images/logo.png" alt="IMG" id='logo'/>
                     </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
