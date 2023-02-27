ce919dac3fcf7c318b6b7312b871bdd9e52a52e9
AdityaCoder-N <91337266+AdityaCoder-N@users.noreply.github.com>
2023-02-27 10:02:17 +0530

    changed services carrousel

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Homepage from '../../components/Homepage/Homepage'
-import ServicesCard from '../../components/ServicesCard/ServicesCard'
+
 import ServicesSection from '../../components/ServicesSection/ServicesSection'
 import Features from '../../components/Features/Features'
 import Clients from '../../components/Clients/Clients'
 import Footer from '../../components/Footer/Footer'
+import ServicesCarrousel from '../../components/ServicesCarrousel/ServicesCarrousel'
 
 export default function Home() {
   return (
       <div>
           <Homepage />
-          <ServicesCard />
+          <ServicesCarrousel />
           <ServicesSection />
           <Features />
           <Clients />
--- src/components/ServicesCard/ServicesCard.jsx
+++ src/components/ServicesCard/ServicesCard.jsx
@@ -1,68 +1,25 @@
-import React ,{useState,useEffect} from 'react'
-import {data} from '../../common/ServiceCardData.js';
+import React from 'react'
+
 import './ServicesCard.css'
-import {setting} from '../../common/setting';
-import Slider from 'react-slick';
 
-const ServicesCard = () => {
-
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 1200);
-    const [hover, setHover] = useState(true)
-    {/* Performs similarly to componentDidMount in classes */}
-    useEffect(() => {
-        window.addEventListener("resize", () => {
-            const ismobile = window.innerWidth < 1200;
-            if (ismobile !== isMobile) setIsMobile(ismobile);
-        }, false);
-    }, [isMobile]);
-
-    console.log(isMobile);
+const ServicesCard = (props) => {
   return (
-    <div className='ServiceCard'>
+    <div className='services-card'>
 
-        <div className='service-content'>
+      <div className='services-card-img-container'>
+        <img src={props.img} alt="" className='services-card-img' />
 
-            <h3> <strong> ___ Our Experties ___</strong> </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam molestiae porro impedit aliquam harum eum doloremque! Pariatur totam excepturi recusandae!</p>
-            
+        <div className='overlay-services-card'>
+          <div className='services-card-content'>{props.content}</div>
         </div>
-    <div className='cards-container' >
-              
-        <Slider {...setting}  >
-            {data.map((element)=>{
-                return(
-                    <div className="service-card-container" key={element.id}>
-                    
-                        <div className='s-card-img-container'>
 
-                            <img src={element.img} alt="img" className='s-card-img'/>
+      </div>
 
-                            <div class="overlay">
-                                <div class="image-text">{element.content}</div>
-                            </div>
+      <div className='services-card-heading'>
+        <h4 >{props.heading}</h4>
+      </div>
 
-
-                        </div>
-
-                        
-                        <div className='s-card-heading'>
-                            <strong>{element.heading}</strong>
-                        </div>
-
-                        
-                        <div className='image-content' style={{display:hover?"block":"none"}} >
-                            <p >{element.content}</p>
-                        </div>
-
-                    </div>
-                   
-                   );
-                })}
-            </Slider>
-        </div>
     </div>
-
-    
   )
 }
 
